b64bb4e1ff5a14aaa706e1c52c3c4df0ce0ce183
Alexander Forselius <drsounds@gmail.com>
2015-03-15 18:15:01 +0100

    Fixed appster

--- templates/object/scripts/main.js
+++ templates/object/scripts/main.js
@@ -2,7 +2,7 @@ window.onargumentschanged = function (event) {
     var arguments = event.data;
     console.log(dpd.<%=model.model%>s);
     dpd.<%-model.model%>s.get(arguments[0]).then (function (<%=model.model%>) {
-        console.log(<%=model.model%>[0]);
-        $('bungalow-header name').html(<%=model.model%>[0].name);
+        console.log(<%=model.model%>);
+        $('bungalow-header name').html(<%=model.model%>.name);
     });
 }
--- templates/objects/scripts/main.js
+++ templates/objects/scripts/main.js
@@ -1,18 +1,53 @@
 window.onargumentschanged = function (event) {
     var arguments = event.data;
     console.log(dpd.<%=model.model%>s);
-    dpd.<%-model.model%>s.get({}).then (function (<%=model.model%>s) {
-     
-        var $tbody = $('table tbody');
-
-        for (var i = 0; i < <%=model.model%>s; i++) {
-            var <%=model.model%> = <%=model.model%>s[i];
-
-            var tr = document.createElement('tr');
-            <% _.each(model.fields, function (field) { %>
-            tr.innerHTML += '<td>' + <%model.model%>.<%=field%> + '</td>';
-            <% }); %>
-            $tbody.append(tr);
-        }
+    dpd.<%-model.model%>s.get(function (<%=model.model%>s) {
+        console.log(<%=model.model%>s);
+        fill_table(<%=model.model%>s);
     });
+}
+
+function fill_table(<%=model.model%>s) {
+    var $tbody = $('table tbody');
+    $tbody.html("");
+
+    for (var i = 0; i < <%=model.model%>s.length; i++) {
+        var <%=model.model%> = <%=model.model%>s[i];
+        console.log(<%=model.model%>);
+        var tr = document.createElement('tr');
+        <% _.each(model.fields, function (field) { %>
+        <% if (field.id == 'name') { %>
+        tr.innerHTML += '<td><a data-uri="bungalow:<%=model.model%>:' + <%=model.model%>.id + '">' + <%=model.model%>.<%=field.id%> + '</a></td>';
+        <% } else { %>
+        tr.innerHTML += '<td>' + <%=model.model%>.<%=field.id%> + '</td>';
+        <% }
+        }); %>
+        tr.innerHTML += '<td><a class="btn btn-danger" onclick="delete(\'' + <%=model.model%>.id + '\')">Delete</a></td>';
+        tr.innerHTML += '<td><a class="btn btn-default" onclick="delete(\'' + <%=model.model%>.id + '\')">View</a></td>';
+        $tbody.append(tr);
+    }
+}
+
+function show_add(event) {
+    $('#form').popover({
+        'anchor': '#addbtn'
+    });
+}
+
+function add(event) {
+    event.preventDefault();
+
+    var <%=model.model%> = {
+        <% _.each(model.fields, function (field) { %>
+        '<%=field.id%>': $('#field_<%=field.id%>').val(),
+        <% });%>
+    };
+    console.log(<%=model.model%>);
+    dpd.<%=model.model%>s.post(<%=model.model%>).then(function (err, done) {
+        dpd.<%=model.model%>s.get(function (<%=model.model%>s) {
+            fill_table(<%=model.model%>s);
+        });
+    });
+    $('#form').fadeOut();
+    return false;
 }
